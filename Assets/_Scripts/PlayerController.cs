f06badb4e541503f5f0ac88a8d795210661f1a8f
Maz2605 <vannam2654@gmail.com>
2024-08-06 00:44:18 +0700

    Base JumpAnim + GroundCheck + fixGaps + Material + Fix Infinity Jump

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float movementInputDirection;
+    private int amountOfJumpsLeft;
+
+    private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    public int amountOfJump = 1;
+    public float jumpForce = 16.0f;
+    public float movementSpeed = 7.0f;
+    public float groundCheckRadius;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJump;
+    }
+
+    private void Update()
+    {
+        CheckInput();
+        CheckMovementDirection();
+        UpdateAnimation();
+        CheckIfCanJump();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+        CheckSurroundings();
+    }
+
+    private void UpdateAnimation()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void CheckMovementDirection() 
+    {
+        if(isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }   
+        else if(!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+
+        if(rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+    //Ground Check
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rb.velocity.y <= 0)
+        {
+            amountOfJumpsLeft = amountOfJump;
+        }
+        if (amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+    }
+    //Di chuyển 
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+    }   
+    //Lật player khi di chuyển trái phải
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+    //Nhảy
+    private void Jump()
+    {
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
+    }
+    //
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
